8ccacd9754db5720dcbc9eccc89c2a7a73329413
Ali Elhenidy <aweka220@YAHOO.COM>
2024-09-16 18:09:37 +0300

    First Try

---
+++ Master.c
@@ -0,0 +1,36 @@
+/*
+ * Master.c
+ *
+ * Created: 9/1/2024 4:50:02 PM
+ *  Author: aweka
+ */ 
+
+
+#include <avr/io.h>
+#include <util/delay.h>
+void Master_init()
+{
+	DDRB |=(1<<4) | (1<<5) | (1<<7); 
+	DDRB &=~ (1<<6); 
+	SPCR |=(1<<SPE) | (1<<MSTR);
+}
+void Master_send(unsigned char data)
+{
+	SPDR  =  data;
+	while((SPSR & (1<<SPIF))==0);
+	//SPSR |=(1<<SPIF);
+}
+
+int main(void)
+{
+	Master_init();
+    while(1)
+    {
+       
+			Master_send ('D');
+			_delay_ms(1000);
+			Master_send (11);
+			_delay_ms(1000);
+		
+    }
+}
