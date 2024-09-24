38d45289d622195a348b7e3b20c83420111d8674
Ali Elhenidy <aweka220@YAHOO.COM>
2024-09-24 18:04:25 +0300

    Delete TIMER0_REV directory

--- TIMER0_REV/TIMER0_REV/Debug/TIMER0.d
+++
@@ -1,8 +0,0 @@
-TIMER0.d TIMER0.o: .././TIMER0.c .././TIMER0_Interface.h \
- .././TIMER0_Private.h .././TIMER0_config.h
-
-.././TIMER0_Interface.h:
-
-.././TIMER0_Private.h:
-
-.././TIMER0_config.h:
